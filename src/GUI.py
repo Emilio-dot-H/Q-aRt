12b3e6d85c4f542cd9ab163e79acbab1893fccde
Emilio Hajj <hajje@mcmaster.ca>
2016-11-19 14:51:21 -0500

    stylized the GUI, added color and logos

--- src/GUI.py
+++ src/GUI.py
@@ -9,58 +9,62 @@ class Application(Frame):
 	def __init__(self, master):
 	    Frame.__init__(self, master)
 	    self.grid()
+	    self.configure(background = 'DeepSkyBlue2')
 	    self.create_widgets()
 
 	def create_widgets(self):
-	    self.instruction = Label(self, text = "Enter URL")
-	    self.instruction.grid(row=1, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.words = Entry(self)
-	    self.words.grid(row =2, column=1, rowspan = 1, sticky=W)
-
-	    self.instruction = Label(self, text = "Enter Version")
+	    self.photo = PhotoImage(file = "Icons/logo.png")
+	    self.logo = Label(self, image = self.photo)
+	    self.logo.grid(row=1, column=1, columnspan = 2, rowspan = 1, sticky = W)
+	    self.instruction = Label(self, text = "Enter URL/Message", bg = 'DeepSkyBlue2')
 	    self.instruction.grid(row=3, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.version = Entry(self)
-	    self.version.grid(row =4, column =1, rowspan = 1, sticky=W)
+	    self.words = Entry(self, width = 30)
+	    self.words.grid(row =4, column=1, sticky=W)
 
-	    self.instruction = Label(self, text = "Enter Level")
+	    self.instruction = Label(self, text = "Enter Version (1-41)", bg = 'DeepSkyBlue2')
 	    self.instruction.grid(row=5, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.level = Entry(self)
-	    self.level.grid(row =6, column =1, rowspan = 1, sticky=W)
+	    self.version = Entry(self, width = 30)
+	    self.version.grid(row =6, column =1, sticky=W)
 
-	    self.instruction = Label(self, text = "Enter Picture")
+	    self.instruction = Label(self, text = "Enter Level (L, M, Q, H)", bg = 'DeepSkyBlue2')
 	    self.instruction.grid(row=7, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.picture = Entry(self)
-	    self.picture.grid(row =8, column =1, rowspan = 1, sticky=W)
+	    self.level = Entry(self, width = 30)
+	    self.level.grid(row =8, column =1, sticky=W)
 
-	    self.instruction = Label(self, text = "Select for color")
+	    self.instruction = Label(self, text = "Enter Media (png, jpg or gif)", bg = 'DeepSkyBlue2')
 	    self.instruction.grid(row=9, column=1, columnspan = 2, rowspan = 1, sticky = W)
+	    self.picture = Entry(self, width = 30)
+	    self.picture.grid(row =10, column =1, sticky=W)
+
+##	    self.instruction = Label(self, text = "Select For Color")
+##	    self.instruction.grid(row=9, column=1, columnspan = 2, rowspan = 1, sticky = W)
 	    self.colVar = BooleanVar()
-	    self.colorized = Checkbutton(self, variable = self.colVar)
-	    self.colorized.grid(row =10, column = 1, rowspan = 1, sticky=W)
+	    self.colorized = Checkbutton(self,text = "Select For Color", variable = self.colVar, bg = 'DeepSkyBlue2')
+	    self.colorized.grid(row =11, column = 1, sticky=W)
 
-	    self.instruction = Label(self, text = "Enter Contrast")
-	    self.instruction.grid(row=11, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.contrast = Entry(self)
-	    self.contrast.grid(row =12, column = 1,rowspan = 1, sticky=W)
+	    self.instruction = Label(self, text = "Enter Contrast (Default: 1.0)", bg = 'DeepSkyBlue2')
+	    self.instruction.grid(row=12, column=1, columnspan = 2, rowspan = 1, sticky = W)
+	    self.contrast = Entry(self, width = 30)
+	    self.contrast.grid(row =13, column = 1, sticky=W)
 
-	    self.instruction = Label(self, text = "Enter Brightness")
-	    self.instruction.grid(row=13, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.brightness = Entry(self)
-	    self.brightness.grid(row =14, column =1, rowspan = 1, sticky=W)
+	    self.instruction = Label(self, text = "Enter Brightness (Default: 1.0)", bg = 'DeepSkyBlue2')
+	    self.instruction.grid(row=14, column=1, columnspan = 2, rowspan = 1, sticky = W)
+	    self.brightness = Entry(self, width = 30)
+	    self.brightness.grid(row =15, column =1, sticky=W)
 
-	    self.instruction = Label(self, text = "Enter File Name")
-	    self.instruction.grid(row=15, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.name = Entry(self)
-	    self.name.grid(row =16, column =1, rowspan = 1, sticky=W)
+	    self.instruction = Label(self, text = "Enter New Name (.png or .gif)", bg = 'DeepSkyBlue2')
+	    self.instruction.grid(row=16, column=1, columnspan = 2, rowspan = 1, sticky = W)
+	    self.name = Entry(self, width = 30)
+	    self.name.grid(row =17, column =1, sticky=W)
 
-	    self.instruction = Label(self, text = "Enter File Location")
-	    self.instruction.grid(row=17, column=1, columnspan = 2, rowspan = 1, sticky = W)
-	    self.directory = Entry(self)
-	    self.directory.grid(row =18, column =1, rowspan = 1, sticky=W)
+	    self.instruction = Label(self, text = "Enter File Location", bg = 'DeepSkyBlue2')
+	    self.instruction.grid(row=18, column=1, columnspan = 2, rowspan = 1, sticky = W)
+	    self.directory = Entry(self, width = 30)
+	    self.directory.grid(row =19, column =1, sticky=W)
 	
 	
 	#Create first button
-	    self.button1 = Button(self, text = "GENERATE", width =10, command = self.reveal)
+	    self.button1 = Button(self, text = "Generate", width =10, command = self.reveal)
 	    self.button1.grid(row=20, column=1, sticky = W)
 
 	def reveal(self):
@@ -74,15 +78,15 @@ class Application(Frame):
 		else:
 			levelStr = self.level.get()
 
-		if self.directory.get() == '':
+		if self.directory.get() == '':  #DEFAULT for blank DIRECTORY input
 			directoryStatus = os.getcwd()
 		else:
 			directoryStatus = self.directory.get()
-		if self.contrast.get() == '':
+		if self.contrast.get() == '':   #DEFAULT for blank Contrast input
 			contrast = 1.0
 		else:
 			contrast = float(self.contrast.get())
-		if self.brightness.get() == '':
+		if self.brightness.get() == '': #DEFAULT for blank Brightness input
 			brightness = 1.0
 		else:
 			brightness = float(self.brightness.get())
@@ -103,7 +107,9 @@ class Application(Frame):
 			raise
 def gui_main():
 	root = Tk()
-	root.title("QR-Encoder")
-	root.geometry("500x500")
+	root.title("Q-aRt Encoder")
+	root.geometry("305x435")
+	root.configure(background = 'DeepSkyBlue2')
+	root.wm_iconbitmap('Icons/paint.ico')
 	app = Application(root)
 	root.mainloop()
